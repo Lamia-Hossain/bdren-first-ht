48d420f3a3c7507a80f8090d9a892c497eb7715b
Lamia Hossain <lamiahossain2017@gmail.com>
2023-05-18 15:19:28 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import proIco from "./figures/LamiaHossain.jpg";
+import "./App.css";
+import logOut from "./icons/log-out-outline.svg";
+import profile from "./icons/person-circle-outline.svg";
+
+import React, { useState, useEffect, useRef } from "react";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  let menuRef = useRef();
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.addEventListener("mousedown", handler);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="App">
+      <header class="App-header">
+        <div class="menu-container" ref={menuRef}>
+          <div
+            class="menu-trigger"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            <img src={proIco} class="proIco" alt="Lamia" />
+          </div>
+
+          <div class={`dropdown-menu ${open ? "active" : "inactive"}`}>
+            <h3>
+              The Kite
+              <br />
+              <span>Website Designer</span>
+            </h3>
+            <ul>
+              <DropdownItem img={profile} text={"Profile"} alt="P" />
+              <DropdownItem img={logOut} text={"Log Out"} alt="P" />
+            </ul>
+          </div>
+        </div>
+
+        <div>
+          <p className="text-2xl">Hello</p>
+        </div>
       </header>
     </div>
+  );
+}
+
+function DropdownItem(props) {
+  return (
+    <li class="dropDownItem">
+      <img src={props.img} alt="P" />
+      <a>{props.text}</a>
+    </li>
   );
 }
 
